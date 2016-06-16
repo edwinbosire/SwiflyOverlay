0b8541062947231e837f28ecc762bfd326f7848f
Edwin Bosire <me@edwinb.co.uk>
2016-06-16 22:37:03 +0100

    Initial Commit

---
+++ TransparentView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransparentView
+//
+//  Created by edwinbosire on 16/06/2016.
+//  Copyright © 2016 Edwin Bosire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
